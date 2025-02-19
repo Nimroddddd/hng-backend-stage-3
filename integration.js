feebd059f64c9cca350118fdf035eb9e411b7d85
Amin <min123kabir@gmail.com>
2025-02-19 02:42:57 +0100

    modify integration file

--- integration.js
+++ integration.js
@@ -1,32 +1,82 @@
 const integrationSettings = {
   "data": {
     "date": {
-      "created_at": "2025-02-28",
+      "created_at": "2025-02-18",
       "updated_at": "2025-02-18"
     },
     "descriptions": {
-      "app_name": "Memecoin trend reminder",
-      "app_description": "Sends updates on memecoin at regular intervals",
+      "app_description": "An integration that shows updatets on memcoin prices",
       "app_logo": "https://imgur.com/a/oAAI53s",
-      "app_url": "https://6dmlhvnn-3000.uks1.devtunnels.ms",
+      "app_name": "Memecoin trend",
+      "app_url": "https://hng-backend-stage-3.onrender.com",
       "background_color": "#fff"
     },
-    "is_active": true,
+    "integration_category": "Data Analytics & Visualization",
     "integration_type": "interval",
-    "key_features": [
-      "shows coin prices"
+    "is_active": true,
+    "output": [
+      {
+        "label": "output_channel_1",
+        "value": true
+      },
+      {
+        "label": "output_channel_2",
+        "value": false
+      }
     ],
-    "author": "Amin",
+    "key_features": [
+      "Feature description 1.",
+      "Feature description 2.",
+      "Feature description 3.",
+      "Feature description 4."
+    ],
+    "permissions": {
+      "monitoring_user": {
+        "always_online": true,
+        "display_name": "Performance Monitor"
+      }
+    },
     "settings": [
       {
-        "label": "Time interval",
+        "label": "interval",
         "type": "text",
         "required": true,
         "default": "* * * * *"
+      },
+      {
+        "label": "Key",
+        "type": "text",
+        "required": true,
+        "default": "1234567890"
+      },
+      {
+        "label": "Do you want to continue",
+        "type": "checkbox",
+        "required": true,
+        "default": "Yes"
+      },
+      {
+        "label": "Provide Speed",
+        "type": "number",
+        "required": true,
+        "default": "1000"
+      },
+      {
+        "label": "Sensitivity Level",
+        "type": "dropdown",
+        "required": true,
+        "default": "Low",
+        "options": ["High", "Low"]
+      },
+      {
+        "label": "Alert Admin",
+        "type": "multi-checkbox",
+        "required": true,
+        "default": "Super-Admin",
+        "options": ["Super-Admin", "Admin", "Manager", "Developer"]
       }
     ],
-    "target_url": "https://6dmlhvnn-3000.uks1.devtunnels.ms/integration.json",
-    "tick_url": "https://6dmlhvnn-3000.uks1.devtunnels.ms/tick"
+    "tick_url": "https://hng-backend-stage-3.onrender.com/tick"
   }
 }
 
